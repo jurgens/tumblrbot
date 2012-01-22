52a10b390556071ab0960a5945e927dddfe3d2da
Yuri Omelchuk <jurgen@upscript.com>
2012-01-22 15:20:17 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tumblrbot::Application.config.secret_token = 'cc21a4e2a9755c1a4a773419e3d8a2cc6ce05904875470f223fbe6d11202e6aa02f001b04eca796047f5a4f64e1272d474440566cd8dd27095ffed556889a4f3'
